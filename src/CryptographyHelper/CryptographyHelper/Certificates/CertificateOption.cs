0276ea91aebbdf06903327f0bc439c9d80b0b7e3
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-02-28 12:06:39 +0700

    Certificate Finding Option

---
+++ src/CryptographyHelper/CryptographyHelper/Certificates/CertificateOption.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CryptographyHelper.Certificates
+{
+    public class CertificateOption
+    {
+        public string Thumbprint { get; set; }
+
+        public string Path { get; set; }
+
+        public string Password { get; set; }
+
+        public X509Certificate2 FindCertificate()
+        {
+            return !string.IsNullOrWhiteSpace(Thumbprint)
+                ? CertificateStore.Find(Thumbprint)
+                : CertificateFile.Find(Path, Password);
+        }
+    }
+}
